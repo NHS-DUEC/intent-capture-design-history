312d672b3f9d3b0cb02b1f3763b3ebad1142ac72
Paul Smith <mail@paulsmith.site>
2026-07-11 17:41:38 +0100

    Add timeline to site navigation

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ export default function (eleventyConfig) {
         items: [
           { text: 'Home', href: '/' },
           { text: 'Posts', href: '/posts/' },
+          { text: 'Timeline', href: '/timeline/' },
           { text: 'User needs', href: '/userneeds/' },
           { text: 'Decisions', href: '/decisions/' },
           { text: 'Concepts', href: '/concepts/' },
